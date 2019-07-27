82487de10bbe41e3d941d5434b70f0f0303149ad
Tim VanDoren <tim.vandoren@tangoanalytics.com>
2019-07-27 11:03:51 -0500

    Implement database for final score and for loanshark forced repayment

--- data.rb
+++ data.rb
@@ -1,3 +1,5 @@
+require 'sequel'
+
 class Data
 
   def self.all_cargo
@@ -21,4 +23,59 @@ class Data
     }
     banned_cargo[cargo] == planet
   end
+end
+
+class DatabaseConnector
+
+  DB = Sequel.sqlite('lokibot.db')
+
+  def initialize
+
+    unless DB.table_exists?(:loanshark)
+      DB.create_table :loanshark do
+        primary_key :id
+        String :game_id
+        Boolean :forced_repayment
+      end
+      puts 'Created new loanshark table'
+    end
+
+    unless DB.table_exists?(:score)
+      DB.create_table :score do
+        primary_key :id
+        String :game_id
+        Integer :final_score
+      end
+      puts 'Created new score table'
+    end
+
+    @loanshark = DB[:loanshark]
+    @score = DB[:score]
+  end
+
+  def add_final_score(game_id, final_score)
+    @score.insert(:game_id => game_id, :final_score => final_score)
+  end
+
+  def get_average_final_score
+    @score.avg(:final_score).round(2)
+  end
+
+  def update_forced_repayment(game_id, did_force_repayment)
+    @loanshark.insert(:game_id => game_id, :forced_repayment => did_force_repayment)
+  end
+
+  def get_percent_forced_repayment
+    forced_repayments = 0
+    total_repayments = 0
+    @loanshark.map([:id, :forced_repayment]).each do |id, repayment|
+      total_repayments += 1
+      if repayment
+        forced_repayments += 1
+      end
+    end
+    puts "forced=#{forced_repayments} total=#{total_repayments}"
+    ((forced_repayments * 1.0) / (total_repayments * 1.0)).round(2)
+  end
+
 end
--- lokibot.rb
+++ lokibot.rb
@@ -8,6 +8,8 @@ SCORE_NAME = 'Loki'
 
 SHOULD_SUBMIT_SCORE = false
 
+DATABASE = DatabaseConnector.new
+
 def create_new_game
   game_data = HTTParty.get('https://skysmuggler.com/game/new_game').parsed_response
   puts "Game ID: #{game_data['gameId']}"
@@ -16,12 +18,14 @@ end
 
 def take_turn(game_data, game_transactions = Transactions.new, travel = Travel.new)
 
+  game_id = game_data['gameId']
+
   game_state = game_data['gameState']
   credits_after_repayment = game_state['credits'] - game_state['loanBalance']
   if game_state['loanBalance'] > 0 and game_state['planet'] == LOAN_SHARK_PLANET and credits_after_repayment > MIN_CREDITS_AFTER_REPAYMENT
     puts "Repaying loan of #{game_state['loanBalance']}, leaving balance of #{credits_after_repayment}"
     game_data = HTTParty.post('https://skysmuggler.com/game/loanshark',
-                              body: {gameId: game_data['gameId'], transaction: {qty: game_state['loanBalance'], side: "repay"}}.to_json)
+                              body: {gameId: game_id, transaction: {qty: game_state['loanBalance'], side: "repay"}}.to_json)
   end
 
   # --- sell
@@ -40,40 +44,54 @@ def take_turn(game_data, game_transactions = Transactions.new, travel = Travel.n
     puts "notifications: #{game_data['notifications']}"
   end
 
-  turns_left = game_state['turnsLeft']
-  current_credits = game_state['credits']
+  turns_left = game_data['gameState']['turnsLeft']
+  current_credits = game_data['gameState']['credits']
   puts "At the end of turn ##{20 - turns_left}, you have #{current_credits} credits"
   puts "Game data: #{game_data}"
   puts
 
+  game_over = false
   # TODO: quit if no credits left because loan shark took them and no cargo left either
   if current_credits == 0
+    game_over = true
     puts 'Putting you out of your misery - you have no credits left'
-    return
+    DATABASE.update_forced_repayment(game_id, true)
+  else
+    if turns_left > 1
+      take_turn(game_data, game_transactions, travel)
+    else
+      game_over = true
+      DATABASE.update_forced_repayment(game_id, false)
+
+      if SHOULD_SUBMIT_SCORE
+        score_response = HTTParty.post('https://skysmuggler.com/scores/submit', body: {gameId: game_data['gameId'], name: 'joe rebel'}.to_json)
+
+        puts "Score response: #{score_response}"
+        if score_response['message'] == 'New high score!'
+          HTTParty.post('https://skysmuggler.com/scores/update_name', body: {gameId: game_data['gameId'], newName: SCORE_NAME}.to_json)
+        else
+          puts "Not a high score"
+        end
+        # https://skysmuggler.com/scores/list?length=10&scoreType[]=allTime&scoreType[]=weekly
+      else
+        puts 'Submitting scores is turned off'
+      end
+
+    end
   end
 
-  if turns_left > 1
-    take_turn(game_data, game_transactions, travel)
-  else
-
-    if SHOULD_SUBMIT_SCORE
-      score_response = HTTParty.post('https://skysmuggler.com/scores/submit', body: {gameId: game_data['gameId'], name: 'joe rebel'}.to_json)
-
-      puts "Score response: #{score_response}"
-      if score_response['message'] == 'New high score!'
-        HTTParty.post('https://skysmuggler.com/scores/update_name', body: {gameId: game_data['gameId'], newName: SCORE_NAME}.to_json)
-      else
-        puts "Not a high score"
-      end
-      # https://skysmuggler.com/scores/list?length=10&scoreType[]=allTime&scoreType[]=weekly
-    else
-      puts 'Submitting scores is turned off'
-    end
+  if game_over
+    DATABASE.add_final_score(game_id, current_credits)
 
     puts "You made #{current_credits - 20000} credits this game"
     puts "You traveled to these planets: #{travel.get_planets_traveled_to}"
     puts "You made these transactions:"
     game_transactions.print_history
+
+    puts
+    puts "All-time stats"
+    puts "Average total score: #{DATABASE.get_average_final_score}"
+    puts "Percent games with loanshark forced repayment: #{DATABASE.get_percent_forced_repayment}%"
   end
 end
 
